5e5e5f42a75b1129d4d9449c5364b30b496c0635
jolusca1 <joaolucasbarroso2000@gmail.com>
2024-08-28 22:10:54 -0300

    Codificando as primeiras models

--- cadastros/admin.py
+++ cadastros/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from cadastros.models import Cliente
 
-# Register your models here.
+class Clientes(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'cpfCnpj', 'tipo', 'endereco')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    
+admin.site.register(Cliente, Clientes)
